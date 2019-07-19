e2e2f8201eba960cbca45d3b778fac26b5448316
Matteo Lucchesi <matteo@luccalug.it>
2019-07-19 13:03:03 +0200

    fix numeric fields content

--- modeltranslation_xliff/utils.py
+++ modeltranslation_xliff/utils.py
@@ -87,7 +87,7 @@ def create_xliff(translation_data):
                     segments = (block,)
                 for seg in segments:
                     skeleton = skeleton.replace(
-                        seg, '%%%{}%%%'.format(segment_id), 1
+                        'value": "' + seg + '"', 'value": "' + '%%%{}%%%'.format(segment_id) + '"', 1
                     )
                     trans_unit = etree.SubElement(
                         inner_group, 'trans-unit', {
